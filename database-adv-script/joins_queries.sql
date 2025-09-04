e5af37a25cf3c9695f209f4f18569ddc95790b63
Okanlawon Kayode Abraham <kayode354@gmail.com>
2025-09-04 09:37:29 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,7 +8,8 @@ SELECT
     u.name AS user_name,
     u.email AS user_email
 FROM bookings b
-INNER JOIN users u ON b.user_id = u.id;
+INNER JOIN users u ON b.user_id = u.id
+ORDER BY b.start_date DESC;
 
 
 -- LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
@@ -19,7 +20,8 @@ SELECT
     r.rating,
     r.comment
 FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+LEFT JOIN reviews r ON p.id = r.property_id
+ORDER BY p.name ASC, r.rating DESC;
 
 
 -- FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking 
@@ -33,7 +35,8 @@ SELECT
     b.start_date,
     b.end_date
 FROM users u
-FULL OUTER JOIN bookings b ON u.id = b.user_id;
+FULL OUTER JOIN bookings b ON u.id = b.user_id
+ORDER BY u.id ASC, b.start_date DESC;
 
 -- Retrieve all properties and their reviews, including properties that have no reviews.
 SELECT p.id AS property_id, p.name AS property_name,

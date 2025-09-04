ff498d1851abd42b28d90353685aa478cda225b2
Okanlawon Kayode Abraham <kayode354@gmail.com>
2025-09-04 09:33:42 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -29,3 +29,15 @@ LEFT JOIN bookings b ON p.id = b.property_id
 GROUP BY p.id, p.name
 ORDER BY property_rank;
 
+-- Window Function (ROW_NUMBER): Assign a unique row number to properties 
+-- ordered by total number of bookings
+SELECT 
+    p.id AS property_id,
+    p.name AS property_name,
+    COUNT(b.id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS row_num
+FROM properties p
+LEFT JOIN bookings b ON p.id = b.property_id
+GROUP BY p.id, p.name
+ORDER BY row_num;
+
